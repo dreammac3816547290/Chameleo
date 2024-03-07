0387ca8df44c7cdb7342774cc226138dd835524b
unknown <nathaniel2048@gmail.com>
2024-03-07 11:32:03 +0800

    V1 update 21, png instead of svg for partnerships images

--- app/ui/Partnerships.js
+++ app/ui/Partnerships.js
@@ -9,10 +9,9 @@ export default function Partnerships() {
       <h2>PARTNERSHIPS</h2>
       <div className={styles.container}>
         {files.map((file) => (
-          <object
+          <img
             key={file}
-            type="image/svg+xml"
-            data={`/partnerships/${file}`}
+            src={`/partnerships/${file}`}
             alt={file}
             className={styles.img}
             style={{ width: "1.00rem", height: "1.00rem" }}
